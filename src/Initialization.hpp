20be265d19608c9c78e3a922bfd49798ccbd2142
louhao088 <louhao088@sjtu.edu.cn>
2025-02-14 17:31:40 +0800

    fix somes bugs in book searching function

--- src/Initialization.hpp
+++ src/Initialization.hpp
@@ -5,5 +5,5 @@ const string USER_FILE = "users.dat";
 const string BOOK_FILE = "books.dat";
 const string FINANCE_LOG = "finance.log";
 const string OPERATION_LOG = "operation.log";
-const int HASH_SIZE = 100003;
+const int HASH_SIZE = 10003;
 const int BLOCK_SIZE = 4096;
